ac3437965f5d216a63bea822c9299c34df88bbe2
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-12 13:43:51 +0400

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 3 -> 1, 4, 9
+// 5 -> 1, 4, 9, 16, 25
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("----------");
+Console.WriteLine(" n  | n^2 ");
+Console.WriteLine("----------");
+
+for (int i = 1; i <= n; i++)
+{
+  Console.WriteLine($"{i, 3} | {i*i, 4}");  
+}
+Console.WriteLine("----------");
